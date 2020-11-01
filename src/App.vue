010e70b2f1922c73e0d5de37692f9ab1b1fff0fd
jayashree-srinivasan <abijay256@gmail.com>
2020-11-01 17:00:53 +0000

    code re-factor

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/customer-contacts.jpg" />
+  <ViewContacts :customer-contacts="customerContacts" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import firebase from "firebase/app";
+import "firebase/firestore";
+import ViewContacts from "./components/ViewContacts.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    ViewContacts,
+  },
+  data() {
+    return {
+      customerContacts: [],
+    };
+  },
+  mounted() {
+    // Get a Firestore instance
+    const db = firebase
+      .initializeApp({ projectId: "customer-contact-vue" })
+      .firestore();
+    db.collection("users")
+      .get()
+      .then((snap) => {
+        const contacts = [];
+        snap.forEach((doc) => {
+          contacts.push(doc.data());
+        });
+        console.log(contacts);
+        this.customerContacts = contacts;
+      });
+  },
+};
 </script>
 
 <style>
---
+++ src/components/ViewContacts.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="hello">
+    <h1>View contact details</h1>
+    <div class="contact-row" v-for="item in customerContacts" :key="item">
+      <div class="contact-item">{{ "Avathar" }}</div>
+      <div class="contact-item">
+        <div class="contact-title">First Name</div>
+        <div class="contact-data">{{ item.FirstName }}</div>
+      </div>
+      <div class="contact-item">
+        <div class="contact-title">Last Name</div>
+        <div class="contact-data">{{ item.LastName }}</div>
+      </div>
+      <div class="contact-item">
+        <div class="contact-title">Phone Number</div>
+        <div class="contact-data">{{ item.ContactNumber }}</div>
+      </div>
+      <div class="contact-item">
+        <div class="contact-title">Email</div>
+        <div class="contact-data">{{ item.Email }}</div>
+      </div>
+      <div class="contact-item">
+        <div class="contact-title">Post code</div>
+        <div class="contact-data">{{ item.PostCd }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ViewContacts",
+  props: {
+    customerContacts: Array,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+.contact-row {
+  display: flex;
+  flex-direction: row;
+  text-align: center;
+  justify-content: center;
+  border: solid 1pt #ff3860;
+  border-radius: 8px;
+  margin: 20px;
+}
+
+@media (max-width: 800px) {
+  .contact-row {
+    flex-direction: column;
+  }
+}
+.contact-item {
+  padding: 10px;
+  flex: 15%;
+}
+
+.contact-title {
+  font-size: 14px;
+  font-style: italic;
+}
+.contact-data {
+  font-size: 25px;
+}
+</style>
